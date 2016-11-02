7fc6b5a04dd2c47cd034730ef070d40419af612a
SHFFLE <mistersh1ffle@gmail.com>
2016-11-02 14:37:40 -0500

    Full second game functionality, with 5 recipes currently working, all needed to add more is just putting them in recipes file. All images for second game added. No sound effects in second game yet.

--- js/recipes.js
+++ js/recipes.js
@@ -3,20 +3,55 @@ var recipes = [
         "id": 1,
         "name": "Bad Touch",
         "recipe": ["Bronson", "Bronson", "Delta", "Delta", "Flanergide", "Flanergide", "Karmotrine", "Karmotrine", "Karmotrine", "Karmotrine", "Mixed", "Rocks"],
-        "recipeStr": "A Bad Touch is <span id='ybronson'>2 Bronson Extract</span>, <span id='bdelta'>2 Powdered Delta</span>, <span id='gflanergide'>2 Flanergide</span>, and <span id='ltbluekarmotrine'>4 Karmotrine</span>. All on the rocks and mixed.",
+        "recipeStr": "A Bad Touch is <span class='ybronson'>2 Bronson Extract</span>, <span class='bdelta'>2 Powdered Delta</span>, <span class='gflanergide'>2 Flanergide</span>, and <span class='ltbluekarmotrine'>4 Karmotrine</span>. All on the rocks and mixed.",
         "description": "&quot;We&#39;re nothing but mammals&quot;",
         "tags": "Sour, Classy, Vintage.",
         "flavor": "sour",
-        "type": "classy"
+        "type": "classy",
+        "img": "<img src='img/badtouch.png'>"
     },
     {
         "id": 2,
         "name": "Beer",
         "recipe": ["Adelhyde", "Bronson", "Bronson", "Delta", "Flanergide", "Flanergide", "Karmotrine", "Karmotrine", "Karmotrine", "Karmotrine", "Mixed"],
-        "recipeStr": "A Beer is <span id='radelhyde'>1 Adelhyde</span>, <span id='ybronson'>2 Bronson Extract</span>, <span id='bdelta'>1 Powdered Delta</span>, <span id='gflanergide'>2 Flanergide</span>, and <span id='ltbluekarmotrine'>4 Karmotrine</span>. All mixed.",
+        "recipeStr": "A Beer is <span class='radelhyde'>1 Adelhyde</span>, <span class='ybronson'>2 Bronson Extract</span>, <span class='bdelta'>1 Powdered Delta</span>, <span class='gflanergide'>2 Flanergide</span>, and <span class='ltbluekarmotrine'>4 Karmotrine</span>. All mixed.",
         "description": "&quot;Traditionally-brewed beer has become a luxury, but this one&#39;s pretty close to the real deal...&quot;",
         "tags": "Bubbly, Classic, Vintage.",
         "flavor": "bubbly",
-        "type": "classic"
+        "type": "classic",
+        "img": "<img src='img/beer.png'>"
+    },
+    {
+        "id": 3,
+        "name": "Bleeding Jane",
+        "recipe": ["Blend", "Bronson", "Delta", "Delta", "Delta", "Flanergide", "Flanergide", "Flanergide", "Mixed"],
+        "recipeStr": "A Bleeding Jane is <span class='ybronson'>1 Bronson Extract</span>, <span class='bdelta'>3 Powdered Delta</span>, and <span class='gflanergide'>3 Flanergide</span>. All blended.",
+        "description": "&quot;Say the name of this drink three times in front of a mirror and you&#39;ll look like a fool.&quot;",
+        "tags": "Spicy, Classic, Sobering.",
+        "flavor": "spicy",
+        "type": "classic",
+        "img": "<img src='img/bleedingjane.png'>"
+    },
+    {
+        "id": 4,
+        "name": "Bloom Light",
+        "recipe": ["Adelhyde", "Adelhyde", "Adelhyde", "Adelhyde", "Age", "Delta", "Flanergide", "Flanergide", "Karmotrine", "Karmotrine", "Karmotrine", "Mixed", "Rocks"],
+        "recipeStr": "A Bloom Light is <span class='radelhyde'>4 Adelhyde</span>, <span class='bdelta'>1 Powdered Delta</span>, <span class='gflanergide'>2 Flanergide</span>, and <span class='ltbluekarmotrine'>3 Karmotrine</span>. All aged, on the rocks, and mixed.",
+        "description": "&quot;It&#39;s so unnecessarily brown...&quot;",
+        "tags": "Spicy, Promo, Bland.",
+        "flavor": "spicy",
+        "type": "promo",
+        "img": "<img src='img/bloomlight.png'>"
+    },
+    {
+        "id": 5,
+        "name": "Blue Fairy",
+        "recipe": ["Adelhyde", "Adelhyde", "Adelhyde", "Adelhyde", "Age", "Flanergide", "Mixed"],
+        "recipeStr": "A Blue Fairy is <span class='radelhyde'>4 Adelhyde</span> and <span class='gflanergide'>1 Flanergide</span>. All aged and mixed.",
+        "description": "&quot;One of these will make all your teeth turn blue. Hope you brushed them well.&quot;",
+        "tags": "Sweet, Girly, Soft.",
+        "flavor": "sweet",
+        "type": "girly",
+        "img": "<img src='img/bluefairy.png'>"
     }
 ];
