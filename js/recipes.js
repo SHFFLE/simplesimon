1e42b8dc9c2d247d8ef7cc45698c6a648c8867a1
SHFFLE <mistersh1ffle@gmail.com>
2016-11-02 09:59:48 -0500

    Basic work on semi-hidden game  added. Formatting is lacking at best, actual "gameplay" (which just consists of adding mixers, hitting mix,  and getting displayed a resulting drink or garbage if it doesn't match a recipe) not implemented

---
+++ js/recipes.js
@@ -0,0 +1,9 @@
+var recipes = [
+    {
+        "id": 1,
+        "name": "Bad Touch",
+        "recipe": ["Bronson", "Bronson", "Delta", "Delta", "Flanergide", "Flanergide", "Karmotrine", "Karmotrine", "Karmotrine", "Karmotrine", "Mixed", "Rocks"],
+        "recipeStr": "A Bad Touch is <span id='ybronson'>2 Bronson Extract</span>, <span id='bdelta'>2 Powdered Delta</span>, <span id='gflanergide'>2 Flanergide</span>, and <span id='ltbluekarmotrine'>4 Karmotrine</span>. All on the rocks and mixed.",
+        "description": "&quot;We&#39;re nothing but mammals&quot;"
+    }
+];
