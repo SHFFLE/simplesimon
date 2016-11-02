89ff724cbe6fffe73ae8ead5c4607deb980919c6
SHFFLE <mistersh1ffle@gmail.com>
2016-11-02 11:39:24 -0500

    Added base functionality to second game, formatting is TODO. Removed background images I decided against, added image files for each ingredient in second game

--- js/recipes.js
+++ js/recipes.js
@@ -4,6 +4,19 @@ var recipes = [
         "name": "Bad Touch",
         "recipe": ["Bronson", "Bronson", "Delta", "Delta", "Flanergide", "Flanergide", "Karmotrine", "Karmotrine", "Karmotrine", "Karmotrine", "Mixed", "Rocks"],
         "recipeStr": "A Bad Touch is <span id='ybronson'>2 Bronson Extract</span>, <span id='bdelta'>2 Powdered Delta</span>, <span id='gflanergide'>2 Flanergide</span>, and <span id='ltbluekarmotrine'>4 Karmotrine</span>. All on the rocks and mixed.",
-        "description": "&quot;We&#39;re nothing but mammals&quot;"
+        "description": "&quot;We&#39;re nothing but mammals&quot;",
+        "tags": "Sour, Classy, Vintage.",
+        "flavor": "sour",
+        "type": "classy"
+    },
+    {
+        "id": 2,
+        "name": "Beer",
+        "recipe": ["Adelhyde", "Bronson", "Bronson", "Delta", "Flanergide", "Flanergide", "Karmotrine", "Karmotrine", "Karmotrine", "Karmotrine", "Mixed"],
+        "recipeStr": "A Beer is <span id='radelhyde'>1 Adelhyde</span>, <span id='ybronson'>2 Bronson Extract</span>, <span id='bdelta'>1 Powdered Delta</span>, <span id='gflanergide'>2 Flanergide</span>, and <span id='ltbluekarmotrine'>4 Karmotrine</span>. All mixed.",
+        "description": "&quot;Traditionally-brewed beer has become a luxury, but this one&#39;s pretty close to the real deal...&quot;",
+        "tags": "Bubbly, Classic, Vintage.",
+        "flavor": "bubbly",
+        "type": "classic"
     }
 ];
